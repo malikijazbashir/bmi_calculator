f700b08efb160beddaf5c737ed3a930109ea2c64
E7440 <malikijazbashir@gmail.com>
2020-11-25 10:05:50 +0500

    Use refactor Code in flutter BMI calculator lecture No 57 part 4

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -18,16 +18,33 @@ class _InputPageState extends State<InputPage> {
           Expanded(child: Row(
             children: <Widget>[
               Expanded(
-                  child: new RepeatContainerCode(colors: Color(0xFF1D1E33))
+                  child: new RepeatContainerCode(colors: Color(0xFF1D1E33),
+                  cardWidget: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.male,
+                        size:80.0,
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text(
+                        'Male',
+                        style: TextStyle(fontSize: 18.0,color: Color(0xFF8D8E98)),
+                      ),
+                    ],
+                  ),
+                  )
               ),
-              Expanded(child: new RepeatContainerCode(colors: Colors.blueGrey,)),
+              Expanded(child: new RepeatContainerCode(colors: Color(0xFF1D1E33),)),
             ],
           )),
-          Expanded(child: new RepeatContainerCode(colors: Colors.greenAccent,)),
+          Expanded(child: new RepeatContainerCode(colors: Color(0xFF1D1E33),)),
           Expanded(child: Row(
             children: <Widget>[
-              Expanded(child: new RepeatContainerCode(colors: Colors.blueAccent,)),
-              Expanded(child: new RepeatContainerCode(colors: Colors.amberAccent,)),
+              Expanded(child: new RepeatContainerCode(colors: Color(0xFF1D1E33),)),
+              Expanded(child: new RepeatContainerCode(colors: Color(0xFF1D1E33),)),
             ],
           )),
         ],
@@ -38,12 +55,14 @@ class _InputPageState extends State<InputPage> {
 }
 
 class RepeatContainerCode extends StatelessWidget {
-  RepeatContainerCode({@required this.colors});
+  RepeatContainerCode({@required this.colors,this.cardWidget});
 final Color colors;
+final cardWidget;
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
+      child: cardWidget,
       decoration: BoxDecoration(
         color:colors,
         borderRadius: BorderRadius.circular(10.0)
